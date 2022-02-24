17f00402672ae1d00da95af99baee880ffb313e5
Sukriti Rai <54813085+SukritiRai0@users.noreply.github.com>
2022-02-24 17:06:02 -0500

    Colored topbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -4,26 +4,15 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import logo from '../logo.svg';
 
 export default function Topbar() {
   return (
     // <img src={logo} className="App-logo" alt="logo" />
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar style={{ background: '#90EC72' }} position="fixed">
         <Toolbar>
         <img src={logo} className="App-logo" alt="logo" width="75"/>
-          {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon /> */}
-          {/* </IconButton> */}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             HackerTime
           </Typography>
