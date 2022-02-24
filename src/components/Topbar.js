99207e45f57ee41a34a61e42cd491ae22ed69883
Sukriti Rai <54813085+SukritiRai0@users.noreply.github.com>
2022-02-24 17:31:51 -0500

    Added copy link functionality
    Needs back end connection

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -6,6 +6,12 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import logo from '../logo.svg';
 
+const copy = async () => {
+    await navigator.clipboard.writeText('link currr');
+    alert('Link copied');
+  }
+
+  
 export default function Topbar() {
   return (
     // <img src={logo} className="App-logo" alt="logo" />
@@ -16,7 +22,9 @@ export default function Topbar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             HackerTime
           </Typography>
-          <Button color="inherit">'Candidate Name'</Button>
+          
+          <Button color="inherit" onClick={copy}>'Link to this page'</Button>
+          <Button color="inherit">Candidate Name</Button>
         </Toolbar>
       </AppBar>
     </Box>
