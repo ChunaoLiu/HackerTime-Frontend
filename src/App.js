aff81513c8f52588872236feb3881bd1e89cf3f8
qiren zhao <zhaoqirenjisoo@gmail.com>
2022-02-23 22:14:53 -0500

    redirection completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-<<<<<<< Updated upstream
 import logo from './logo.svg';
-import React, { useState } from "react";
+import  { useState } from "react";
 import './App.css';
 import Main from './Main'
 import  IDE  from './components/IDE'
-import { Dropdown } from "./components/Dropdown";
-import { Editor } from "./components/Editor";
-import { Highlighter } from "./components/Highlighter";
-import { Button, Checkbox, Form } from 'semantic-ui-react'
 
-=======
-import './App.css';
 import * as React from 'react';
 import {Candidate} from './Candidate'
->>>>>>> Stashed changes
-
+import ReactDOM from "react-dom";
 // import Box from '@mui/material/Box';
 // import TextField from '@mui/material/TextField';
 
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+  BrowserRouter,
+} from "react-router-dom";
+  
 import * as themes from "react-syntax-highlighter/dist/esm/styles/hljs";
 import * as languages from "react-syntax-highlighter/dist/esm/languages/hljs";
 
@@ -26,82 +25,21 @@ const defaultLanguage = <code>${"javascript" || Object.keys(languages).sort()[0]
 const defaultTheme = <code>${"atomOneDark" || Object.keys(themes).sort()[0]}</code>;
 
 export default function App() {
-  /*
+  
 
   const [input, setInput] = useState("");
   const [language, setLanguage] = useState("");
   const [theme, setTheme] = useState("");
   return (
-<<<<<<< Updated upstream
+    <BrowserRouter>
+      <Routes>
+        <Route path="/HackerTime-Frontend/interview" element={<IDE />} />
+        <Route path="/HackerTime-Frontend/" element={<Candidate />} />
+      </Routes>
+    </BrowserRouter>
     
-    < className="App">
-      <div className="ControlsBox">
-      <Dropdown
-          defaultTheme={defaultLanguage}
-          onChange={(e) => setLanguage(e.target.value)}
-          data={languages}
-        />
-        <Dropdown
-          defaultTheme={defaultTheme}
-          onChange={(e) => setTheme(e.target.value)}
-          data={themes}
-        />
-      </div>
-      <div className="PanelsBox">
-      <Editor
-          placeHolder="Type your code here..."
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <Highlighter language={language} theme={themes[theme]}>
-          {input}
-        </Highlighter>
-      </div>
-      <Main/> 
-      
-      
-*/
-    const sendRequest = async () => {
-
-    }
-return (
-    <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome Candidate!
-        </p>
-        {/* <TextField id="name" 
-        label="Filled" variant="filled" /> */}
-
-    <Form>
-        <Form.Field align='left'>
-          <label>First Name</label>
-          <input placeholder='First Name' />
-        </Form.Field>
-        <Form.Field align='left'>
-          <label>Last Name</label>
-          <input placeholder='Last Name' />
-        </Form.Field>
-        <Form.Field>
-          <Checkbox label='Allow Camera and Microphone access' />
-        </Form.Field>
-        <Button type='submit' onPress={sendRequest}>Submit</Button>
-      </Form>
-
-      </header>
-      
-  
-    
-   
-     
-
-  
-  
-    
-    
-=======
-    <Candidate/>
->>>>>>> Stashed changes
   );
 
 }
+ReactDOM.render(<App />, document.getElementById("root"));
 
