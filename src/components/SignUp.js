1cbe5a5a4421b234076154b7ea8e01c22918fc04
Sukriti Rai <54813085+SukritiRai0@users.noreply.github.com>
2022-04-01 00:05:48 -0400

    SignUp working without authentication

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,104 +1,94 @@
 import logo from '../logo.svg';
 import '../App.css';
-import * as React from 'react';
 import { Button, Checkbox, Form } from 'semantic-ui-react'
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-// import {Profile} from './components/profile'
+import React, { useRef, useState, useEffect } from "react";
 
-// import Box from '@mui/material/Box';
-// import TextField from '@mui/material/TextField';
+const SignUp = () => {
+  const navigate = useNavigate();
 
-export default class SignUp extends React.Component{
-  state = {
-    Name: '',
-		Email: '',
-		Password: '',
-		CompanyName: ''
+  const routeChange = () => {
+    let path = "/HackerTime-Frontend/profile";
+    navigate(path);
+  }
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [companyName, setCompanyName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleChangeName = event => {
+    setName(event.target.value);
+  }
+  const handleChangeEmail = event => {
+    setEmail(event.target.value);
+  }
+  const handleChangeCName = event => {
+    setCompanyName(event.target.value);
+  }
+  const handleChangePw = event => {
+    setPassword(event.target.value);
   }
 
-  //let navigate = useNavigate();
-
-  // handleChange = event => {
-  //   this.setState({ name: event.target.value });
-  // }
-
-  // handleSubmit = event => {
-  //   event.preventDefault();
-
-  //   const user = {
-  //     name: this.state.name
-  //   };
-
-  //   axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
-  //     .then(res => {
-  //       console.log(res);
-  //       console.log(res.data);
-  //     })
-  // }
-
-  routeChange = () =>{ 
-    // let path = "/HackerTime-Frontend/profile";
-    // navigate(path);
-  }
-
-  handleChange = event => {
-    this.setState({ name: event.target.value });
-  }
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
     const user = {
-      Name: this.state.Name,
-      Email: this.state.Email,
-      Password: this.state.Password,
-      CompanyName: this.state.CompanyName
+      name: name,
+      email: email,
+      password: password,
+      companyName: companyName
     };
+    console.log(user);
 
-    axios.post('http://localhost:8080/v1/auth/signup', { user })
+    axios.post('http://localhost:8080/v1/auth/signup', {
+      name: name,
+      email: email,
+      password: password,
+      companyName: companyName
+    })
       .then(res => {
         console.log(res);
         console.log(res.data);
       })
+      routeChange();
   }
-render() {
   return (
     <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" width="200"/>
+        <img src={logo} className="App-logo" alt="logo" width="200" />
         <p align='left'>
-        SIGN UP
+          SIGN UP
         </p>
 
-    <Form onSubmit={this.handleSubmit}>
-        <Form.Field align='left'>
-          <label>Name</label>
-          <input placeholder='Name' onChange={this.handleChange}/>
-        </Form.Field>
-        <Form.Field align='left'>
-          <label>Email</label>
-          <input placeholder='Email' onChange={this.handleChange}/>
-        </Form.Field>
-        <Form.Field align='left'>
-          <label>Company Name</label>
-          <input placeholder='Company Name' onChange={this.handleChange}/>
-        </Form.Field>
-        <Form.Field align='left'>
-          <label>Password</label>
-          <input placeholder='Password' onChange={this.handleChange}/>
-        </Form.Field>
-        <Form.Field align='left'>
-          <input placeholder='Retype Password' />
-          <input type="checkbox" id="scales" name="scales"></input>
-         <label>Agree with terms and conditions</label>
-        </Form.Field>
-        {/* <Button type='submit' onClick={routeChange}>Create Account</Button> */}
-        <Button type='submit'>Create Account</Button>
-      </Form>
+        <Form onSubmit={handleSubmit}>
+          <Form.Field align='left'>
+            <label>Name</label>
+            <input placeholder='Name' onChange={handleChangeName} />
+          </Form.Field>
+          <Form.Field align='left'>
+            <label>Email</label>
+            <input placeholder='Email' onChange={handleChangeEmail} />
+          </Form.Field>
+          <Form.Field align='left'>
+            <label>Company Name</label>
+            <input placeholder='Company Name' onChange={handleChangeCName} />
+          </Form.Field>
+          <Form.Field align='left'>
+            <label>Password</label>
+            <input placeholder='Password' onChange={handleChangePw} />
+          </Form.Field>
+          <Form.Field align='left'>
+            <input placeholder='Retype Password' />
+            <input type="checkbox" id="scales" name="scales"></input>
+            <label>Agree with terms and conditions</label>
+          </Form.Field>
+          {/* <Button type='submit' onClick={routeChange}>Create Account</Button> */}
+          <Button type='submit'>Create Account</Button>
+        </Form>
       </header>
     </div>
   );
 }
-}
 
+export default SignUp;
